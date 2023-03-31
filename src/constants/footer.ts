1e27ff35d2f3a11f7ed1d3b735724795ef880937
maxwellwachira <maxwellwachira67@gmail.com>
2023-03-31 08:01:09 +0300

    footer links

---
+++ src/constants/footer.ts
@@ -0,0 +1,38 @@
+export const footerData = [
+    {
+        title: 'Site Map', 
+        links: [
+            { id: 0, link: '/', label: 'Home' },
+            { id: 1, link: '/about', label: 'About' },
+            { id: 2, link: '/book-online', label: 'Book Online' },
+            { id: 3, link: '/learning-resources', label: 'Learning Resources' }
+        ]
+    },
+    {
+        title: 'Blogs', 
+        links: [
+            { id: 0, link: '/blogs', label: 'Blogs Page' },
+            { id: 1, link: '#', label: 'Authentic Learning' },
+            { id: 2, link: '#', label: 'GMAT' },
+            { id: 2, link: '#', label: 'IELTS AND TOEFL' },
+        ]
+    }, 
+    {
+        title: 'Contact', 
+        links: [
+            { id: 0, link: '/contact', label: 'Contact Page' },
+            { id: 1, link: '/faq', label: 'FAQ Page' },
+            { id: 2, link: 'mailto:info@raenalearning.org', label: 'Email' },
+            { id: 2, link: 'mailto:scholarship@ategoschool.com', label: 'WhatsApp' },
+        ]
+    }, 
+    {
+        title: 'Social Links',
+        links: [
+            { id: 0, link: '#', label: 'Twitter' },
+            { id: 1, link: '#', label: 'LinkedIn' },
+            { id: 2, link: '#', label: 'Instagram' },
+            { id: 3, link: '#', label: 'YouTube' }
+        ]
+    },
+];
