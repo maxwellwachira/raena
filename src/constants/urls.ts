8fda5e0ed0cce0fc941f054185e302564f3fcbf3
maxwellwachira <maxwellwachira67@gmail.com>
2023-12-08 20:26:26 +0300

    updated url

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -1,5 +1,5 @@
 export const urls = {
     frontendUrl: "http://localhost:3000",
-    strapiUrl: "http://localhost:1337/api",
+    strapiUrl: "http://85.90.246.243:1337/api",
     strapiBaseUrl: "http://localhost:1337"
 }
